ffafb63c956adfdb0a04b46affab5f4c710006f4
Nima Khatamsazan <akhatamsazan@myseneca.ca>
2021-10-19 20:33:53 -0400

    Initial commit

---
+++ models/mealkit.js
@@ -0,0 +1,84 @@
+var fastfood= [ 
+    pizza={
+    title:"Pizza Peperoni",
+    included:"Delicious Italian Cheese & peperoni",
+    description: "Tasty Pizza",
+    category: "Fast Food",
+    price: "13$",
+    cookingTime: "10 Minutes",
+    Calories: '850cal',
+    imageURL:"pizzaPeperoni.jpg",
+    topMeals: true,
+        },
+         burger= {
+            title:"King Burger",
+            included:"Famous American Cheese & Organic Meat",
+            description: "Tasty burger",
+            category: "Fast Food",
+            price: "8$",
+            cookingTime: "8 Minutes",
+            Calories: '750cal',
+            imageURL:"KingBurger.jpg",
+
+            topMeals: true,
+                },
+                 chickenBurger= {
+                    title:"Chicken Burger",
+                    included:"The Best Taste of Domestic Chicken",
+                    description: "Tasty Chicken",
+                    category: "Fast Food",
+                    price: "7$",
+                    cookingTime: "9 Minutes",
+                    Calories: '650cal',
+                    imageURL:"ChickenBurger.jpg",
+                    topMeals: true,
+                        },
+                         pasta= {
+                            title:"Pasta",
+                            included:"Real Alfredo Pasta & chicken",
+                            description: "Tasty pasta",
+                            category: "Fast Food",
+                            price: "8$",
+                            cookingTime: "10 Minutes",
+                            Calories: '840cal',
+                            imageURL:"pasta.png",
+
+                            topMeals: false,
+                                },
+                                 hotDog= {
+                                    title:"Hot Dog",
+                                    included:"Real American Hot dog",
+                                    description: "Tasty hot dog",
+                                    category: "Fast Food",
+                                    price: "2$",
+                                    cookingTime: "5 Minutes",
+                                    Calories: '350cal',
+                                    imageURL:"hotDog.jpg",
+                                    topMeals: false,
+                                        },
+                                         wings= {
+                                            title:"Wings",
+                                            included:"Organic Wings",
+                                            description: "Tasty Wing",
+                                            category: "Fast Food",
+                                            price: "15$",
+                                            cookingTime: "20 Minutes",
+                                            Calories: '550cal',
+                                            imageURL:"wings.jpg",
+                                            topMeals: false,
+                                                },
+];
+                                            module.exports.getAllMealKit = function(){
+return fastfood;
+                                            };
+
+                                            module.exports.getTopMeals = function(){
+                                                var filtered=[];
+                                                for (var i=0; i < fastfood.length; i++){
+if (fastfood[i].topMeals){
+    filtered.push(fastfood[i]);
+}
+                                                }
+                                                return filtered;
+                                            }
+                                       
---
+++ server.js
@@ -0,0 +1,102 @@
+/************************************************************************************
+*  WEB322 – Project (Fall 2021)
+*  I declare that this assignment is my own work in accordance with Seneca Academic
+*  Policy. No part of this assignment has been copied manually or electronically from
+*  any other source (including web sites) or distributed to other students.
+*  
+*  Name: Nima Khatamsazan
+*  Student ID: 136034204
+*  Course/Section: WEB322 ZAA
+* 
+************************************************************************************/
+
+const path = require("path");
+const express = require ("express");
+const exphbs = require('express-handlebars')
+const app = express();
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine','.hbs');
+const mealkitModel= require("./models/mealkit")
+app.use(express.static("static"));
+
+//set up home page
+
+app.get("/", (req,res)  =>{
+   
+
+    res.render("home", {
+        fastfood : mealkitModel.getTopMeals(),
+                layout:false
+    });
+
+});
+
+app.get("/menu", (req,res)  =>{
+                                   
+    res.render("onTheMenu", {
+       fastfood : mealkitModel.getAllMealKit(),
+       layout:false
+
+    });
+
+});
+
+
+
+app.get("/pricing", (req,res)  =>{
+
+    const headers = req.headers;
+    res.send(headers);
+});
+app.get("/Blog", (req,res)  =>{
+
+    const headers = req.headers;
+    res.send(headers);
+});
+
+app.get("/login", (req,res)  =>{
+    
+
+    res.render("login", {
+    
+        layout:false
+    });
+});
+app.get("/signUp", (req,res)  =>{
+    
+
+    res.render("signUp", {
+    
+        layout:false
+    });
+});
+// *** THE FOLLOWING CODE SHOULD APPEAR IN YOUR ASSIGNMENT AS IS (WITHOUT MODIFICATION) ***
+
+// This use() will not allow requests to go beyond it
+// so we place it at the end of the file, after the other routes.
+// This function will catch all other requests that don't match
+// any other route handlers declared before it.
+// This means we can use it as a sort of 'catch all' when no route match is found.
+// We use this function to handle 404 requests to pages that are not found.
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+// This use() will add an error handler function to
+// catch all errors.
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+});
+
+// Define a port to listen to requests on.
+const HTTP_PORT = process.env.PORT || 8080;
+
+// Call this function after the http server starts listening for requests.
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+  
+// Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
+// because sometimes port 80 is in use by other applications on the machine
+app.listen(HTTP_PORT, onHttpStart);
