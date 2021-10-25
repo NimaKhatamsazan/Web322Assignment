9ec83d22ce0c742bb05d33ea1a13ca7b6136962c
Nima Khatamsazan <akhatamsazan@myseneca.ca>
2021-10-25 11:46:56 -0400

    deploy sign up and login pages

--- controllers/general.js
+++ controllers/general.js
@@ -1,90 +1,130 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-
 // Login page
-router.get("/loginPage", (req, res) => {
-    res.render("login", {
-        title: "login Page",
-       
-    });
+router.get('/loginPage', (req, res) => {
+  res.render('login', {
+    title: 'login Page',
+  });
 });
 
+router.post('/loginPage', (req, res) => {
+  const { email, psw } = req.body;
 
-router.post("/loginPage", (req, res) => {
-    res.json(req.body);
-    
+  let passed = true;
+  let validation = {};
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,12})/;
+
+  if (
+    typeof email !== 'string' ||
+    email.trim().length === 0 ||
+    !emailRegex.test(email)
+  ) {
+    passed = false;
+    validation.email = 'You must enter a correct email address';
+  } else if (
+    typeof psw !== 'string' ||
+    psw.trim().length === 0 ||
+    !passwordRegex.test(psw)
+  ) {
+    passed = false;
+    validation.password = 'You must enter a strong password';
+  }
+
+  if (passed) {
+    res.redirect('/welcome');
+  } else {
+    res.render('login', {
+      values: req.body,
+      validation,
+      layout: false,
+    });
+  }
 });
 
 // Sign Up page
-router.get("/signUp", (req, res) => {
-    res.render("/signUp", {
-        title: "Sign Up Page",
-       
+router.get('/signUp', (req, res) => {
+  res.render('/signUp', {
+    title: 'Sign Up Page',
+  });
+});
+
+router.post('/signUp', (req, res) => {
+  console.log(req.body);
+  const { uname, lname, email, psw } = req.body;
+
+  let passed = true;
+  let validation = {};
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,12})/;
+
+  if (typeof uname !== 'string' || uname.trim().length === 0) {
+    passed = false;
+    validation.uname = 'You must enter your First name';
+  } else if (typeof uname !== 'string' || uname.trim().length < 2) {
+    passed = false;
+    validation.uname = 'Your First name must be more than 2 characters';
+  } else if (
+    typeof email !== 'string' ||
+    email.trim().length === 0 ||
+    !emailRegex.test(email)
+  ) {
+    passed = false;
+    validation.email = 'You must enter a correct email address';
+  } else if (
+    typeof psw !== 'string' ||
+    psw.trim().length === 0 ||
+    !passwordRegex.test(psw)
+  ) {
+    passed = false;
+    validation.password = 'You must enter a strong password';
+  }
+
+  if (passed) {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    const msg = {
+      to: email,
+      from: 'akhatamsazan@myseneca.ca',
+      subject: 'Canada Eats Sign Up',
+      html: `Vistor's Full Name: ${uname} ${lname}<br>
+                Vistor's Email Address: ${email}<br>                `,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        // Validation passed, sent out an email.
+        // res.send('Success, validation passed, email sent.');
+        res.redirect('/welcome');
+      })
+      .catch(err => {
+        console.log(`Error ${err}`);
+
+        res.render('signUp', {
+          values: req.body,
+          layout: false,
+          validation,
+        });
+      });
+  } else {
+    res.render('signUp', {
+      values: req.body,
+      validation,
+      layout: false,
     });
+  }
 });
 
-
-router.post("/signUp", (req, res) => {
-    console.log(req.body);
-    const { uname, lname, email, psw } = req.body; 
-
-    let passed = true;
-    let validation={};
-    if (typeof uname !== 'string' || uname.trim().length === 0){
-        passed= false;
-        validation.uname="You must enter your First name"
-    }
-    else if (typeof uname !== 'string' || uname.trim().length < 2){
-        passed= false;
-        validation.uname="Your First name must be more than 2 characters"
-
-    }
-    if(passed){
-        const sgMail = require("@sendgrid/mail");
-        sgMail.setApiKey('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');
-
-        const msg = {
-            to: email,
-            from: 'akhatamsazan@myseneca.ca',
-            subject: 'Canada Eats Sign Up',
-            html:
-                `Vistor's Full Name: ${uname} ${lname}<br>
-                Vistor's Email Address: ${email}<br>                `
-        };
-        sgMail.send(msg)
-            .then(() => {
-                // Validation passed, sent out an email.
-                res.send("Success, validation passed, email sent.");
-            })
-            .catch(err => {
-                console.log(`Error ${err}`);
-
-                res.render("signUp", {
-                    values : req.body,
-    
-                    layout:false,
-                    validation
-            
-                });
-           
-            });
-    }
-
-    else{
-       
-
-            res.render("signUp", {
-                values : req.body,
-
-                layout:false
-            });
-       
-       
-    }
-    
+router.get('/welcome', (req, res, next) => {
+  res.render('welcome', { layout: false });
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,125 +1,103 @@
 /************************************************************************************
-*  WEB322 – Project (Fall 2021)
-*  I declare that this assignment is my own work in accordance with Seneca Academic
-*  Policy. No part of this assignment has been copied manually or electronically from
-*  any other source (including web sites) or distributed to other students.
-*  
-*  Name: Nima Khatamsazan
-*  Student ID: 136034204
-*  Course/Section: WEB322 ZAA
-* 
-************************************************************************************/
-const dotenv = require('dotenv');
-dotenv.config({path:"keys.env"});
-const path = require("path");
-const express = require ("express");
-const exphbs = require('express-handlebars');
-const app = express();
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'home'}, ));
-app.set('view engine','.hbs');
-const mealkitModel= require("./models/mealkit")
-app.use(express.static("static"));
-const bodyParser = require('body-parser');
-// Set up body parser
-app.use(bodyParser.urlencoded({ extended: false }));
-//set up home page
-
-//set up home page
-
-app.get("/", (req,res)  =>{
-   
-
-    res.render("home", {
-        fastfood : mealkitModel.getTopMeals(),
-                layout:false
-    });
-
-});
-
-app.get("/menu", (req,res)  =>{
-                                   
-    res.render("onTheMenu", {
-       fastfood : mealkitModel.getAllMealKit(),
-       layout:false
-
-    });
-
-});
-
-
-
-app.get("/pricing", (req,res)  =>{
-
-    const headers = req.headers;
-    res.send(headers);
-});
-app.get("/Blog", (req,res)  =>{
-
-    const headers = req.headers;
-    res.send(headers);
-});
-
-app.get("/login", (req,res)  =>{
-    
-
-    res.render("login", {
-    
-        layout:false
-    });
-    
-    
-});
-app.get("/welcome", (req,res)  =>{
-    
-
-    res.render("welcome", {
-    
-        layout:false
-    });
-    
-    
-});
-app.get("/signUp", (req,res)  =>{
-    
-
-    res.render("signUp", {
-    
-        layout:false
-    });
-});
-
-// Configure my controllers.
-const generalController = require("./controllers/general");
-
-app.use("/", generalController);
-
-// *** THE FOLLOWING CODE SHOULD APPEAR IN YOUR ASSIGNMENT AS IS (WITHOUT MODIFICATION) ***
-
-// This use() will not allow requests to go beyond it
-// so we place it at the end of the file, after the other routes.
-// This function will catch all other requests that don't match
-// any other route handlers declared before it.
-// This means we can use it as a sort of 'catch all' when no route match is found.
-// We use this function to handle 404 requests to pages that are not found.
-app.use((req, res) => {
-    res.status(404).send("Page Not Found");
-});
-
-// This use() will add an error handler function to
-// catch all errors.
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send("Something broke!")
-});
-
-// Define a port to listen to requests on.
-const HTTP_PORT = process.env.PORT || 8080;
-
-// Call this function after the http server starts listening for requests.
-function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
-}
-  
-// Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
-// because sometimes port 80 is in use by other applications on the machine
-app.listen(HTTP_PORT, onHttpStart);
+ *  WEB322 – Project (Fall 2021)
+ *  I declare that this assignment is my own work in accordance with Seneca Academic
+ *  Policy. No part of this assignment has been copied manually or electronically from
+ *  any other source (including web sites) or distributed to other students.
+ *
+ *  Name: Nima Khatamsazan
+ *  Student ID: 136034204
+ *  Course/Section: WEB322 ZAA
+ *
+ ************************************************************************************/
+ const dotenv = require('dotenv');
+ dotenv.config();
+ const path = require('path');
+ const express = require('express');
+ const exphbs = require('express-handlebars');
+ const app = express();
+ app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'home' }));
+ app.set('view engine', '.hbs');
+ const mealkitModel = require('./models/mealkit');
+ app.use(express.static('static'));
+ const bodyParser = require('body-parser');
+ // Set up body parser
+ app.use(bodyParser.urlencoded({ extended: false }));
+ //set up home page
+ 
+ //set up home page
+ 
+ app.get('/', (req, res) => {
+   res.render('home', {
+     fastfood: mealkitModel.getTopMeals(),
+     layout: false,
+   });
+ });
+ 
+ app.get('/menu', (req, res) => {
+   res.render('onTheMenu', {
+     fastfood: mealkitModel.getAllMealKit(),
+     layout: false,
+   });
+ });
+ 
+ app.get('/pricing', (req, res) => {
+   const headers = req.headers;
+   res.send(headers);
+ });
+ app.get('/Blog', (req, res) => {
+   const headers = req.headers;
+   res.send(headers);
+ });
+ 
+ app.get('/login', (req, res) => {
+   res.render('login', {
+     layout: false,
+   });
+ });
+ app.get('/welcome', (req, res) => {
+   res.render('welcome', {
+     layout: false,
+   });
+ });
+ app.get('/signUp', (req, res) => {
+   res.render('signUp', {
+     layout: false,
+   });
+ });
+ 
+ // Configure my controllers.
+ const generalController = require('./controllers/general');
+ 
+ app.use('/', generalController);
+ 
+ // *** THE FOLLOWING CODE SHOULD APPEAR IN YOUR ASSIGNMENT AS IS (WITHOUT MODIFICATION) ***
+ 
+ // This use() will not allow requests to go beyond it
+ // so we place it at the end of the file, after the other routes.
+ // This function will catch all other requests that don't match
+ // any other route handlers declared before it.
+ // This means we can use it as a sort of 'catch all' when no route match is found.
+ // We use this function to handle 404 requests to pages that are not found.
+ app.use((req, res) => {
+   res.status(404).send('Page Not Found');
+ });
+ 
+ // This use() will add an error handler function to
+ // catch all errors.
+ app.use(function (err, req, res, next) {
+   console.error(err.stack);
+   res.status(500).send('Something broke!');
+ });
+ 
+ // Define a port to listen to requests on.
+ const HTTP_PORT = process.env.PORT || 8080;
+ 
+ // Call this function after the http server starts listening for requests.
+ function onHttpStart() {
+   console.log('Express http server listening on: ' + HTTP_PORT);
+ }
+ 
+ // Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
+ // because sometimes port 80 is in use by other applications on the machine
+ app.listen(HTTP_PORT, onHttpStart);
+ 
